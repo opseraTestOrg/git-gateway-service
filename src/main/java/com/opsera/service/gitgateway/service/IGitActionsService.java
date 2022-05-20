28f11fd31164aa3d5e0755b4618b440eafef0fac
DESKTOP-HBKL430\Lenovo <hemadri.bheemavaram@opsera.io>
2022-05-20 23:19:08 +0530

    OPL-3318 cleanup

--- src/main/java/com/opsera/service/gitgateway/service/IGitActionsService.java
+++
@@ -1,9 +0,0 @@
-package com.opsera.service.gitgateway.service;
-
-import com.opsera.service.gitgateway.resources.GitGatewayRequest;
-import com.opsera.service.gitgateway.resources.GitGatewayResponse;
-
-public interface IGitActionsService {
-    GitGatewayResponse createPullRequest(GitGatewayRequest request);
-    GitGatewayResponse createTag(GitGatewayRequest request);
-}
